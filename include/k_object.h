db94b47f13b326c9996a44bb839e4e124be6b8d7
Keith Leonardo <kleonardo52@gmail.com>
2020-12-14 11:07:09 +1100

    Implement simple objects as wrappers for datatypes

---
+++ include/k_object.h
@@ -0,0 +1,43 @@
+#ifndef K_OBJECT_H_
+#define K_OBJECT_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Define the atomic data types.
+ * More complex data structures can be 
+ * built on top of this.
+ */
+typedef enum {
+    K_INTEGER,
+    K_FLOAT,
+    K_BOOL,
+    K_STRING,
+    K_ARRAY,
+    K_HASHTABLE
+} KObjectType;
+
+/**
+ * Every atomic datatype can be expressed
+ * as a k_object_t pointer by converting it.
+ * This includes variable length types like
+ * arrays and hashtables.
+ */
+typedef struct {
+    void *data;
+    size_t length;
+    KObjectType type;
+} k_object_t;
+
+
+k_object_t *object_create(void *data, size_t length, KObjectType type);
+
+void object_destroy(k_object_t *object);
+
+k_object_t *object_clone(k_object_t *parent);
+
+unsigned object_id(k_object_t *object);
+
+#endif
---
+++ src/k_object.c
@@ -0,0 +1,37 @@
+#include "k_object.h"
+#include "k_array.h"
+
+k_object_t *
+object_create(void *data, size_t length, KObjectType type) {
+    k_object_t *object = malloc(sizeof(k_object_t));
+    assert(object);
+
+    object->data = malloc(length);
+    assert(object->data);
+
+    object->length = length;
+    object->type = type;
+    memcpy(object->data, data, length);
+
+    return object;
+}
+
+void
+object_destroy(k_object_t *object) {
+    free(object->data);
+    free(object);
+}
+
+k_object_t *
+object_clone(k_object_t *parent) {
+    return object_create(
+        parent->data,
+        parent->length,
+        parent->type
+    );
+}
+
+unsigned
+object_id(k_object_t *object) {
+    return (uintptr_t)object;
+}

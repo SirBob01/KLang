7a450e33dcded8dcc680ca4d81e27fd0c27a69ba
Keith Leonardo <kleonardo52@gmail.com>
2023-05-29 03:04:55 +1000

    Parse program arguments

--- src/Klang.cpp
+++ src/Klang.cpp
@@ -1,3 +1,64 @@
-#include "./klang.hpp"
+#include "./Klang.hpp"
 
-int main(int argc, char **argv) { return 0; }
+void log_error(std::string error) { throw std::runtime_error(error); }
+
+void print_usage() {
+    log_error("Usage: klang -i <input_file> -o <output_file>");
+}
+
+Args parse_args(int argc, char **argv) {
+    Args args;
+
+    // Get string views of the command line arguments
+    std::vector<std::string_view> cmd_args(argv, argv + argc);
+    if (cmd_args.size() != 3 && cmd_args.size() != 5) {
+        print_usage();
+    }
+
+    // Read the contents of the input file
+    if (cmd_args[1] == "-i") {
+        std::ifstream file(cmd_args[2].data(), std::ios::in);
+        std::stringstream stream;
+        if (!file.is_open()) {
+            log_error("Could not open input file: " + std::string(cmd_args[2]));
+        }
+        stream << file.rdbuf();
+        args.input = stream.str();
+    } else {
+        print_usage();
+    }
+
+    // Optionally, open the output file
+    if (cmd_args.size() == 5) {
+        if (cmd_args[3] == "-o") {
+            args.output_file = cmd_args[4].data();
+        } else {
+            print_usage();
+        }
+    }
+    return args;
+}
+
+int main(int argc, char **argv) {
+    try {
+        Args args = parse_args(argc, argv);
+
+        // TODO: Run compiler arguments
+        std::string compiled;
+
+        // Write to output buffer
+        if (args.output_file.length() > 0) {
+            std::ofstream file(args.output_file, std::ios::out);
+            if (!file.is_open()) {
+                log_error("Could not open output file: " + args.output_file);
+            }
+            file << compiled;
+        } else {
+            std::cout << compiled << std::endl;
+        }
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
--- src/Klang.hpp
+++ src/Klang.hpp
@@ -1,3 +1,30 @@
 #pragma once
 #define KLANG_VERSION_MAJOR 0
 #define KLANG_VERSION_MINOR 1
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <vector>
+
+/**
+ * @brief Program arguments.
+ *
+ */
+struct Args {
+    std::string input;
+    std::string output_file;
+};
+
+/**
+ * @brief Get the input string and output buffer (if provided) from the command
+ * line arguments.
+ *
+ * @param argc
+ * @param argv
+ * @return Args
+ */
+Args parse_args(int argc, char **argv);
